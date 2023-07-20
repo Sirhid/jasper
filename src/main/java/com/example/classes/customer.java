a67657fb772d64895292a460d19233c5a7802f6f
saheed alabi <halfheart6767@yahoo.com>
2023-07-20 17:39:43 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/com/example/classes/customer.java
@@ -0,0 +1,41 @@
+package com.example.classes;
+
+public class customer {
+
+    private String userid;
+    private String customername;
+    private String dob;
+    private String phonenumber;
+
+    public String getuserid() {
+        return userid;
+    }
+
+    public void setuserid(String iban) {
+        this.userid = userid;
+    }
+
+    public String getcustomername() {
+        return customername;
+    }
+
+    public void set(String customername) {
+        this.customername = customername;
+    }
+
+    public String getphonenumber() {
+        return phonenumber;
+    }
+
+    public void setphonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+    public String getdob() {
+        return dob;
+    }
+
+    public void setdob(String dob) {
+        this.dob = dob;
+    }
+
+}
